7ec8446760e576cf99058bdb1d8757dc2745aba6
Alon Harmon <alonharmon@pop-os.localdomain>
2022-01-19 17:53:15 +0200

    fix server main

--- Server/Program.cs
+++ Server/Program.cs
@@ -9,7 +9,7 @@ namespace Server
             var ipAddress = IPAddress.Parse(args[0]); 
             int port = int.Parse(args[1]);
             var server = new MirrorServer(1024);
-            server.Start(6060, ipAddress);
+            server.Start(port, ipAddress);
         }
     }
 }
