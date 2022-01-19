b1ab60a7ecc99945c414cc182600bb6a327fa481
Alon Harmon <alonharmon@pop-os.localdomain>
2022-01-19 17:37:01 +0200

    Add SocketClient

--- Client/SocketClient.cs
+++ Client/SocketClient.cs
@@ -1,7 +1,44 @@
-namespace .
+using System.Security.AccessControl;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Client.Abstractions;
+using System.Net;  
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client
 {
-    public class SocketClient
+    public class SocketClient : IClient
     {
-        
+        public int BufferSize { get; set; }
+        private byte[] _buffer;
+        private int _port;
+        private Socket _socket;
+
+        public SocketClient(int bufferSize, IPAddress ipAddress, int port)
+        {
+            BufferSize = bufferSize;
+            _buffer = new byte[bufferSize];
+            _socket = new Socket(ipAddress.AddressFamily,
+                SocketType.Stream, ProtocolType.Tcp );
+            _port = port;
+        }
+
+        public void Connect()
+        {
+            IPEndPoint remoteEndPoint = new IPEndPoint(((IPEndPoint)(_socket.RemoteEndPoint)).Address, _port);
+            _socket.Connect(remoteEndPoint);
+        }
+        public string Receive()
+        {
+            int byteReceived = _socket.Receive(_buffer);
+            return Encoding.ASCII.GetString(_buffer,0,byteReceived);
+        }
+
+        public void Send(string message)
+        {
+            _socket.Send(Encoding.ASCII.GetBytes(message));
+        }
     }
 }
