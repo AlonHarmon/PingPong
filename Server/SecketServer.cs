c803bbc03c6aa5839956059973e4d63ada6053b2
Alon Harmon <alonharmon@pop-os.localdomain>
2022-01-20 10:18:20 +0200

    change Mirror server to Socket server that implemant Iserver

---
+++ Server/SecketServer.cs
@@ -0,0 +1,59 @@
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.Net;  
+using System.Net.Sockets;
+using Server.Abstractions;
+using System.Collections.Concurrent;
+
+namespace Server
+{
+    public class MirrorServer : IServer
+    {
+        public int BufferSize { get; set; }
+        private byte[] _buffer;
+        private ConcurrentDictionary<Guid, Socket> _sockets;
+        public MirrorServer(int bufferSize)
+        {
+            BufferSize = bufferSize;
+            _buffer = new byte[bufferSize];
+        }
+        public void Start(int port, IPAddress ipAddress, Action<IServer> clientHandler)
+        {
+            var endPoint = new IPEndPoint(ipAddress, port);
+            var listener = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            listener.Bind(endPoint);
+            listener.Listen(10);
+
+            while (true)
+            {
+                Socket handler = listener.Accept();
+                _sockets[Guid.NewGuid()] = handler;
+                clientHandler?.Invoke(this);
+            }
+        }
+
+        private async Task ClientHandler(Socket handler)
+        {
+            while(true)
+            {
+                await Task.Delay(1000);
+                int bytesReceived = handler.Receive(_buffer);
+
+                handler.Send(_buffer);
+                Array.Clear(_buffer, 0, _buffer.Length);
+            }
+        }
+
+        public string Receive(Guid connection)
+        {
+            int bytesReceived = _sockets[connection].Receive(_buffer);
+            return Encoding.ASCII.GetString(_buffer, 0, bytesReceived);
+        }
+
+        public void Send(Guid connection, string message)
+        {
+            _sockets[connection].Send(Encoding.UTF8.GetBytes(message));
+        }
+    }
+}
