467c8aeb5f70500e1d35c6351c811eacd31ee464
Alon Harmon <alonharmon@pop-os.localdomain>
2022-01-20 09:57:00 +0200

    Add CLient main

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,12 +1,19 @@
-﻿using System;
+﻿using System.Net.WebSockets;
+using System.Net.Http;
+using System.Net;
+using System.Threading.Tasks;
 
 namespace Client
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var ipAddress = IPAddress.Parse(args[0]); 
+            int port = int.Parse(args[1]);
+            var socketClient = new SocketClient(1024, ipAddress, port);
+            EchoClient client = new EchoClient(socketClient, new ConsoleInput(), new ConsoleOutput());
+            await client.Start();
         }
     }
 }
